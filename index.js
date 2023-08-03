09a06dfc3272c363132d525614fadea576e73c8a
itsYashASeeker <yashkc6432@gmail.com>
2023-08-03 22:08:29 +0530

    c home2

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = 5013;
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors({ credentials: true, origin: process.env.frontEndLink }));
+app.use(cors());
 connectDB();
 
 app.get("/dbdb/yo", () => {
